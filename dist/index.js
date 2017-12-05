d89ee659672c271432b2125b5c718694691f89c7
Johannes Bader <jogus@live.de>
2017-12-05 13:53:03 -0800

    initial

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var extension_base_1 = require("./lib/extension-base");
+exports.AutoRestExtension = extension_base_1.AutoRestExtension;
